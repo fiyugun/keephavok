34d2ed9bc699842262748addde7070c044124c1f
Soujiro Seta <56367843+fiyugun@users.noreply.github.com>
2025-04-09 21:48:03 -0500

    index.php
    archivo de php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saludo en PHP</title>
+</head>
+<body>
+
+<?php
+    // Mensaje de saludo
+    echo "<h1>¡Hola, bienvenido a mi página PHP en Azure!</h1>";
+?>
+
+</body>
+</html>
